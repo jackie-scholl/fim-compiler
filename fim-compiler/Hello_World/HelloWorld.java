59b6b95a8486fcd3d731ed9e71e75a5234d04229
Jackson <shades97@gmail.com>
2012-10-28 14:33:17 -0400

    No change.

---
+++ fim-compiler/Hello_World/HelloWorld.java
@@ -0,0 +1,16 @@
+import com.twitter.raptortech97.git.rand.fimcompiler.Princess_Celestia;
+// AUTO-GENERATED CLASS
+public class HelloWorld extends Princess_Celestia{
+public static void main(String[] args){ an_Important_Lesson_about_Friendship(); }
+public static void an_Important_Lesson_about_Friendship(){
+
+ String Spike="Hello World!";
+System.out.println(Spike);
+
+ double Applejack=67.428;
+System.out.println(Applejack);
+
+}
+
+// This is a comment.
+} // Author: Twilight Sparkle
